d9298aa41914012b90e19c6a6c768cd936c0b33c
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-07-02 08:53:55 +0200

    機能を完成

--- src/messageCreate/expandTwitter.js
+++ src/messageCreate/expandTwitter.js
@@ -1,4 +1,4 @@
-const { Client, Message, MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const { Client, Message } = require("discord.js");
 const displus = require("displus");
 
 /**
@@ -9,5 +9,43 @@ const displus = require("displus");
  */
 module.exports = (client, message, config) => {
   if (message.author.bot) return;
-  // Devloping...
+
+    if (!message?.content) return;
+    const content = displus.removeHiddenText(message.content);
+    const regex = /(https:\/\/(?:x\.com|twitter\.com)\/\w+\/status\/\w+)/g;
+    const urls = content.match(regex);
+
+    const fixedContent = [];
+    for (const url of urls) {
+      const parseRegex = /(?:x\.com|twitter\.com)\/(\w+)\/status/;
+      const username = url.match(parseRegex)[1];
+      const fixedUrl = `https://fxtwitter.com/${username}/status/${url.split("/").pop()}`;
+      const fixed = `[Tweet @${username}](${fixedUrl})`;
+      fixedContent.push(fixed);
+    }
+
+    if (fixedContent.length > 0) {
+      let isSent = false;
+      try {
+        message.channel.send({
+          content: fixedContent.join("\n"),
+          allowedMentions: {
+            parse: [] // メンションを無効化
+          }
+        });
+        isSent = true;
+      } catch(e) {
+        console.error(e);
+      }
+
+      setTimeout(() => {
+        if (isSent) {
+          try {
+            message.suppressEmbeds(true);
+          } catch (e) {
+            console.error(e);
+          }
+        }
+      }, 300);
+    }
 };
